d82476023e7feb215016d6f965988939fa99a364
Sagitov.V <v.sagitov@bk.ru>
2020-04-13 00:56:50 +0500

    Добавлено подключение к базе, роли, angular фронт.

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using ContactsApp.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactsApp.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        //public ApplicationContext()
+        //{
+        //    Database.EnsureCreated();
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=contactsdb;Username=postgres;Password=password");
+        }
+    }
+}
